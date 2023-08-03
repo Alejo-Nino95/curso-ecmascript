674cb191128150e35d32321bda1ec1e0199a2569
Alejo-Nino95 <alejo_120295@hotmail.com>
2023-08-03 10:43:41 -0500

    Todos los ejercicios

---
+++ src/ES11/01-bigint.js
@@ -0,0 +1,5 @@
+const aBigNumber = 732872307892378937n
+const anotherBigNumber = BigInt(732872307892378937)
+
+console.log(aBigNumber)
+console.log(anotherBigNumber)
---
+++ src/ES6/07-clases.js
@@ -0,0 +1,34 @@
+class User {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+    greeting() {
+        return `${this.speak()} ${this.name}`
+    }
+    speak() {
+        return "Hello"
+    }
+
+    get uAge() {
+        return this.age
+    }
+
+    set uAge(n) {
+        this.age = n
+    }
+}
+
+const newUser = new User("Ana");
+
+console.log(newUser.greeting())
+
+const bebeloper = new User();
+
+//setters getters
+
+const david = new User("David", 15)
+console.log(david.uAge)
+console.log(david.uAge = 20)
+console.log(david.uAge)
+
